aaf8da7d25d3dfee3c995c54e199f959bd5e47f7
Timothy Otieno <timothyking094@gmail.com>
2020-09-16 13:21:28 +0300

    Modified the UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
 
+//Import Global State
+import GlobalState from "./context/GlobalState";
+
+//Import Components
+import {
+  Header,
+  Balance,
+  IncomeExpenses,
+  TransactionList,
+  AddTransaction,
+} from "./components/exportFiles";
+
 function App() {
-  return <div>Hellow World!</div>;
+  return (
+    <GlobalState>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalState>
+  );
 }
 
 export default App;
